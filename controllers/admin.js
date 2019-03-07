2927c1afd5067b61c5933d2b3ebee5c6878b3cbf
Archaeologist03 <omniiknight@hotmail.com>
2019-03-07 23:22:35 +0100

    Creating user model. Adding one to many relationship. Creating dummy user. Using magic associ methods. Fethcing related products. 161.

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title,
-    price,
-    imageUrl,
-    description,
-  })
+  req.user
+    .createProduct({
+      title,
+      price,
+      imageUrl,
+      description,
+    })
     .then(result => {
       console.log('CReated Product');
       res.redirect('/admin/products');
@@ -34,8 +35,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findById(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -72,7 +76,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  res.user
+    .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
