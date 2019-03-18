e327c7998e64567dbcbb2f0e5f41690720371671
Archaeologist03 <omniiknight@hotmail.com>
2019-03-18 23:45:08 +0100

    Adding edit and delete btns again. Editing and deleting products(products model). Adding User model and storing user to db.

--- models/product.js
+++ models/product.js
@@ -2,20 +2,27 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? mongodb.ObjectID(id) : null;
+    this.userId = userId;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+    let dbOp;
+    if (this._id) {
+      //update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp.then(res => console.log(res)).catch(err => console.log(err));
   }
 
   static fetchAll() {
@@ -40,6 +47,17 @@ class Product {
       .then(product => {
         console.log(product);
         return product;
+      })
+      .catch(err => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectID(prodId) })
+      .then(result => {
+        console.log('deleted');
       })
       .catch(err => console.log(err));
   }
