c57b2660ac5b93dff0ccb64382864f74a900c553
Archaeologist03 <omniiknight@hotmail.com>
2019-03-19 21:58:57 +0100

    Working on cart items, adding add to cart functionality. 193.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5c9019d0c162775b13c45963')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
