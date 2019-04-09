52658d45984c480526816fbfb551cc40ca18a15b
Archaeologist03 <omniiknight@hotmail.com>
2019-04-09 11:24:50 +0200

    Pagination. Retriving chunk of data, preparing pagination data on server, adding dynamic pagination buttons. Finished 21th section, Pagination.

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,14 +6,31 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
+        pageTitle: 'Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
@@ -33,16 +50,36 @@ exports.getProduct = (req, res, next) => {
         path: '/products',
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
@@ -80,6 +117,11 @@ exports.postCart = (req, res, next) => {
     .then(result => {
       console.log(result);
       res.redirect('/cart');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -165,14 +207,13 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
-      pdfDoc.fontSize(26).text('invoice', {
+      pdfDoc.fontSize(26).text('Invoice', {
         underline: true,
       });
-
-      pdfDoc.text('----------------');
+      pdfDoc.text('-----------------------');
       let totalPrice = 0;
       order.products.forEach(prod => {
-        totalPrice += prod.product.price * prod.quantity;
+        totalPrice += prod.quantity * prod.product.price;
         pdfDoc
           .fontSize(14)
           .text(
@@ -188,7 +229,6 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
       pdfDoc.end();
-
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
       //     return next(err);
@@ -196,11 +236,10 @@ exports.getInvoice = (req, res, next) => {
       //   res.setHeader('Content-Type', 'application/pdf');
       //   res.setHeader(
       //     'Content-Disposition',
-      //     'inline; filename="' + invoiceName + '"',
+      //     'inline; filename="' + invoiceName + '"'
       //   );
       //   res.send(data);
       // });
-
       // const file = fs.createReadStream(invoicePath);
 
       // file.pipe(res);
